3c5361364765896941905f550960dbc2c8707e67
MAYANK5634 <71888040+MAYANK5634@users.noreply.github.com>
2021-04-18 13:22:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+//Create variables here
+var dod,dogimg,happydogimg,database,foodStock,foodS;
+function preload()
+{
+	//load images here
+  dogimg = loadImage("images/dogImg.png")
+  happydogimg = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  database = firebase.database();
+  foodStock = database.ref("Food");
+  foodStock.on("value",readStock);
+  foodStock.set(20);
+
+  dog = createSprite(250,350,10,60);
+  dog.addImage(dogimg);
+  dog.scale = 0.2;
+}
+
+
+function draw() {  
+background("blue");
+if(foodS!== undefined){
+
+textSize(20);
+fill("orange");
+text("Note: Press UP ARROW key to feed Drago milk",50,50);
+text("Food Remaining:"+foodS,150,150);
+
+
+if(keyWentDown(UP_ARROW)){
+
+writeStock(foodS);
+dog.addImage(happydogimg);
+
+}
+
+if(keyWentUp(UP_ARROW)){
+
+dog.addImage(dogimg);
+
+}
+
+if(foodS===0){
+
+  foodS = 20;
+}
+
+}
+
+
+  drawSprites();
+  //add styles here
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+
+    x=0;
+
+  }
+  else{
+    x = x-1;
+  }
+
+  database.ref("/").update({
+
+    Food:x
+  });
+}
+
+
